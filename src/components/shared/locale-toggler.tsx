6d663b33300267cd5b5442955528df494e64c498
Guilherme Alves <97guilherme.alves@gmail.com>
2024-12-20 22:53:06 -0300

    new corrections basoc

--- src/components/shared/locale-toggler.tsx
+++ src/components/shared/locale-toggler.tsx
@@ -15,12 +15,12 @@ export default function LocaleToggler() {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="sm" className="w-9 px-0">
           <LanguagesIcon className="h-5 w-5" />
-          <span className="sr-only">Language Options</span>
+          <span className="sr-only">Opções de Linguagem</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem disabled>
-          <span>English (default)</span>
+          <span>Português (default)</span>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
