fc707bd151f49e9bdb47eab22f670946f967f4d0
Guilherme Alves <97guilherme.alves@gmail.com>
2024-12-22 17:13:46 -0300

    new corrections in modal login

--- src/components/layout/login-modal.tsx
+++ src/components/layout/login-modal.tsx
@@ -10,12 +10,26 @@ import {
 } from "@/components/ui/credenza";
 import { Button } from "@/components/ui/button";
 import AuthForm from "./auth-form";
+import { useEffect } from "react";
 
 export default function LoginModal() {
   const router = useRouter();
   const pathname = usePathname();
 
   const isOpen = pathname.includes("/login");
+
+  useEffect(() => {
+    // Evitar rolagem no corpo ao abrir o modal
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+
+    return () => {
+      document.body.style.overflow = ""; // Limpa ao desmontar
+    };
+  }, [isOpen]);
 
   return (
     <Credenza open={isOpen} onOpenChange={() => router.back()}>
@@ -25,7 +39,7 @@ export default function LoginModal() {
       >
         Login
       </Button>
-      <CredenzaContent className="mx-auto my-12 h-fit gap-0 border bg-background p-0 max-w-[400px]">
+      <CredenzaContent className="mx-auto my-12 h-fit gap-0 border bg-background p-0 max-w-[400px] focus:outline-none">
         <CredenzaHeader className="border-b p-5">
           <CredenzaTitle className="font-semibold text-foreground">
             Entrar
