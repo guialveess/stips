34a38310e4d2e604f3afa9f5cd8cf0cac6ea0147
Guilherme Alves <97guilherme.alves@gmail.com>
2024-12-23 18:49:09 -0300

    modal login mudado

--- src/components/layout/login-modal.tsx
+++ src/components/layout/login-modal.tsx
@@ -2,56 +2,46 @@
 
 import { usePathname, useRouter } from "next/navigation";
 import {
-  Credenza,
-  CredenzaContent,
-  CredenzaDescription,
-  CredenzaHeader,
-  CredenzaTitle,
-} from "@/components/ui/credenza";
-import { Button } from "@/components/ui/button";
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import AuthForm from "./auth-form";
-import { useEffect } from "react";
 
 export default function LoginModal() {
   const router = useRouter();
   const pathname = usePathname();
 
-  const isOpen = pathname.includes("/login");
-
-  useEffect(() => {
-    // Evitar rolagem no corpo ao abrir o modal
-    if (isOpen) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "";
-    }
-
-    return () => {
-      document.body.style.overflow = ""; // Limpa ao desmontar
-    };
-  }, [isOpen]);
-
+  const IsOpen = pathname.includes("/login");
   return (
-    <Credenza open={isOpen} onOpenChange={() => router.back()}>
-      <Button
-        disabled={false}
-        className="hidden" // O botão fica oculto, já que a abertura depende da rota
-      >
-        Login
-      </Button>
-      <CredenzaContent className="mx-auto my-12 h-fit gap-0 border bg-background p-0 max-w-[400px] focus:outline-none">
-        <CredenzaHeader className="border-b p-5">
-          <CredenzaTitle className="font-semibold text-foreground">
-            Entrar
-          </CredenzaTitle>
-          <CredenzaDescription className="font-medium text-muted-foreground">
-            Por favor, faça login para continuar.
-          </CredenzaDescription>
-        </CredenzaHeader>
-        <div className="space-y-4 p-5">
+    <Dialog open={IsOpen} onOpenChange={() => router.back()}>
+      <DialogContent className="w-full max-w-[400px] rounded-lg shadow-lg borde">
+        <DialogHeader className="text-center">
+          <DialogTitle asChild>
+            <h2 className="font-bold text-2xl text-white tracking-tight">
+              Bem-vindo de volta!
+            </h2>
+          </DialogTitle>
+          <p className="text-sm text-gray-500 mt-2">
+            Faça login na sua conta para continuar.
+          </p>
+        </DialogHeader>
+        <div className="px-6 py-4">
           <AuthForm />
         </div>
-      </CredenzaContent>
-    </Credenza>
+        <div className="mt-4 text-center text-sm text-gray-500">
+          <p>
+            Não tem uma conta?{" "}
+            <a
+              href="/"
+              className="text-blue-500 hover:underline font-medium"
+            >
+              Registre-se agora
+            </a>
+          </p>
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 }
